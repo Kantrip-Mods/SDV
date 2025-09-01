e50263521de566b44dc166a4435131f900de29bf
Kantrip <nada>
2025-09-01 12:53:51 -0700

    Added info message for proposal timing to help users

--- ReverseProposals/ReverseProposals/ModEntry.cs
+++ ReverseProposals/ReverseProposals/ModEntry.cs
@@ -5,6 +5,7 @@
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using ReverseProposals.SweetTokens;
+using StardewValley;
 
 namespace ReverseProposals;
 
@@ -51,7 +52,7 @@ public class ModEntry : Mod
         RegisterActions();
 
         Globals.Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
-        //Globals.Helper.Events.GameLoop.DayStarted += OnDayStarted;
+        Globals.Helper.Events.GameLoop.DayStarted += OnDayStarted;
         Globals.Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
     }
 
@@ -61,10 +62,46 @@ public class ModEntry : Mod
         RegisterTokens();
     }
 
-    //private static void OnDayStarted(object? sender, DayStartedEventArgs e)
-    //{
-    //    Globals.Monitor.Log($"MM: OnDayStarted", LogLevel.Debug);
-    //}
+    private static void OnDayStarted(object? sender, DayStartedEventArgs e)
+    {
+        //Globals.Monitor.Log($"MM: OnDayStarted", LogLevel.Debug);
+        Farmer farmer = Game1.player;
+        String CPModID = "Kantrip.MarryMe";
+        foreach (string name in farmer.friendshipData.Keys)
+        {
+            NPC npc = Game1.getCharacterFromName(name);
+            if (npc == null)
+            {
+                continue;
+            }
+
+            Friendship friendship = farmer.friendshipData[name];
+            if (npc.isMarried() || !friendship.IsDating())
+            {
+                continue;
+            }
+
+            string timeKey = CPModID + "_Timer_" + npc.Name;
+            string startKey = CPModID + "_Start" + npc.Name;
+            string whiteKey = CPModID + "_Proposal_" + npc.Name;
+            if (farmer.activeDialogueEvents.ContainsKey(timeKey))
+            {
+                int days = farmer.activeDialogueEvents[timeKey];
+                Globals.Monitor.Log($"{npc.getName()} will be ready to propose in {days} days", LogLevel.Info);
+            }
+            else if (farmer.mailReceived.Contains(startKey))
+            {
+                if (farmer.eventsSeen.Contains(whiteKey))
+                {
+                    Globals.Monitor.Log($"{npc.getName()} already proposed", LogLevel.Info);
+                }
+                else
+                {
+                    Globals.Monitor.Log($"{npc.getName()} is ready to propose", LogLevel.Info);
+                }
+            }
+        }
+    }
 
     private static void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
     {
